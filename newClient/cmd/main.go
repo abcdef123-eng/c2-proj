bc739c49d40c6f3a4a1b32b0d9474daae5958c71
callumakroyd <callumakroyd06@gmail.com>
2026-03-25 15:04:25 +0000

    .

--- newClient/cmd/main.go
+++ newClient/cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/execute-assembly/c2-proj/newClient/internal/client"
+	//"github.com/execute-assembly/c2-proj/newClient/internal/client"
+	"github.com/execute-assembly/c2-proj/newClient/internal/rpc"
 )
 
 func main() {
-
-	client.RunClient()
+	rpc.RunRpcClient()
+	//client.RunClient()
 }
